9fe05c5c2c58709f2b6899d63aff6c72e2fd7866
madhavmore23445-saka <madhavmore23445@gmail.com>
2026-05-30 18:01:32 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+// live - https://smart-deal-finder-chatbot-backend-2.vercel.app/
+// local - http://localhost:5001
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://10.61.141.48:5001',
         changeOrigin: true,
       },
     },
